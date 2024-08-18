322d966ab560909dfa7447b553c56fc88849045d
Nik <Nik96.ge@gmail.com>
2024-08-18 15:50:00 +0330

    Enforce braces for all statements

--- src/lints/javascript/index.js
+++ src/lints/javascript/index.js
@@ -1,5 +1,9 @@
 export default {
   // https://eslint.org/docs/latest/rules/eqeqeq
   // Require the use of === and !==
-  eqeqeq: ["error"]
+  eqeqeq: ["warn"],
+
+  // https://eslint.org/docs/latest/rules/curly
+  // Enforce consistent brace style for all control statements
+  curly: ["warn", "all"]
 };
