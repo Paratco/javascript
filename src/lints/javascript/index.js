dea38dfd723679a8b8493e43061bf4d8879c5a64
Nik <Nik96.ge@gmail.com>
2025-01-22 14:56:09 +0330

    Disallow node:process import

--- src/lints/javascript/index.js
+++ src/lints/javascript/index.js
@@ -5,5 +5,19 @@ export default {
 
   // https://eslint.org/docs/latest/rules/curly
   // Enforce consistent brace style for all control statements
-  curly: ["warn", "all"]
+  curly: ["warn", "all"],
+
+  // https://eslint.org/docs/latest/rules/no-restricted-imports
+  // Disallow specified modules when loaded by import
+  "no-restricted-imports": [
+    "error",
+    {
+      patterns: [
+        {
+          regex: "(node:)?process",
+          message: "Please dont import node:process."
+        }
+      ]
+    }
+  ]
 };
