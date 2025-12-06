1885e8a74df7e60deb01416c6ede50f818fadbb7
Peyman <peiman_3009@yahoo.com>
2025-12-06 17:56:01 +0330

    feat: added commonjs exports

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,10 +1,28 @@
 // eslint-disable-next-line import-x/no-extraneous-dependencies
 import { defineConfig } from "tsdown";
 
-export default defineConfig({
-  entry: "./src/index.ts",
-  platform: "node",
-  target: "node22",
-  minify: true,
-  sourcemap: true
-});
+export default defineConfig([
+  {
+    entry: "./src/index.ts",
+    format: {
+      cjs: {},
+      esm: {}
+    },
+    dts: false,
+    platform: "node",
+    target: "node22",
+    minify: true,
+    sourcemap: true
+  },
+  {
+    entry: "./src/index.ts",
+    dts: {
+      oxc: true,
+      emitDtsOnly: true
+    },
+    outputOptions: {
+      entryFileNames: "[name].js"
+    },
+    target: false
+  }
+]);
